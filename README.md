d0dae409a6303a01583da8398fad51106fae2291
Pablo <pabloquispe3366@gmail.com>
2024-06-24 07:08:01 -0500

    mi tarea

---
+++ README.md
@@ -0,0 +1,3 @@
+# Improved Flask App
+
+This is a simple Flask application with a more organized structure.
---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template, request
+from app import app
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        return render_template('contact.html', success=True)
+    return render_template('contact.html')
